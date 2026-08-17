cdea47a5293db9753d8120eb652ecaee1fcc22c5
Camille JOLLIET <cjolliet@laprovence.com>
2026-08-17 10:06:33 +0200

    3 - Placement Peblob sur cellule

--- src/world/world.module.ts
+++ src/world/world.module.ts
@@ -4,12 +4,13 @@ import { WorldController } from './world.controller';
 import { WorldGateway } from './world.gateway';
 import { WorldService } from './world.service';
 import { Cell, CellSchema } from './schemas/cell.schema';
+import { HttpJwtGuard } from '../auth/http-jwt.guard';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Cell.name, schema: CellSchema }]),
   ],
   controllers: [WorldController],
-  providers: [WorldGateway, WorldService],
+  providers: [WorldGateway, WorldService, HttpJwtGuard],
 })
 export class WorldModule {}
